8fae22825079835986f4d39293fc72d42cd47c96
jalondono <juanlondono151776@hotmail.com>
2019-09-10 00:31:39 +0000

    upload advanced task

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 import sys
-sys.stdout.write(str("and that piece of art is useful - Dora Korpar, 2015-10-19") + '\n')
+sys.stderr.write(str("and that piece of art is useful - Dora Korpar, 2015-10-19") + '\n')
 sys.exit(1)
