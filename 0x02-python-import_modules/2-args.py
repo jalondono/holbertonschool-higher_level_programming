4cbecfd2bbba3a8e0e4bd11c4b0ef5335b191aad
jalondono <juanlondono151776@hotmail.com>
2019-09-12 19:39:29 +0000

    upload task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 j = 0
-size = len(sys.argv) - 1
+size = len(argv) - 1
 if size == 0:
         print("{:d} arguments.".format(size))
 else:
@@ -9,7 +9,7 @@ else:
         print("{:d} argument:".format(size))
     else:
         print("{:d} arguments:".format(size))
-for i in sys.argv:
-    if i != sys.argv[0]:
+for i in argv:
+    if i != argv[0]:
         print("{:d}: {}".format(j, i))
     j += 1
