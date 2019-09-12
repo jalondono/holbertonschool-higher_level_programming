f84725f02c3981ff93052cb99fed7940dae480e3
jalondono <juanlondono151776@hotmail.com>
2019-09-12 14:53:56 +0000

    try solving the last check

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -53,7 +53,7 @@ listint_t *insert_node(listint_t **head, int number)
 			copy->next = new;
 			return (new);
 		}
-		else if (auxvalue >= number && i == 0)
+		else if (auxvalue >= number && i == 0 || copy == NULL)
 		{
 			new->next = *head;
 			*head = new;
