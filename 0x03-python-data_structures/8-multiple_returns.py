7883270760097271bd3f326e7d0bec30224bb493
jalondono <juanlondono151776@hotmail.com>
2019-09-17 19:57:03 +0000

    upload task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,8 @@ def multiple_returns(sentence):
     if sentence is None:
         return 0, None
     size = len(sentence)
+    if size == 0:
+        char = ''
+        return size, char
     char = sentence[0]
     return size, char
