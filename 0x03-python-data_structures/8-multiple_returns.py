0a7da159717ae957a8e6e03e0408d9dc6c3ef20a
jalondono <juanlondono151776@hotmail.com>
2019-09-17 19:59:11 +0000

    upload task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        return 0, None
+        return 0, "None"
     size = len(sentence)
     if size == 0:
-        char = ''
+        char = 'None'
         return size, char
     char = sentence[0]
     return size, char
