c0e35ff4395022e08a5826dcc66d2bfecc069589
jalondono <juanlondono151776@hotmail.com>
2019-09-19 14:24:13 -0500

    upload task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     if my_list is None:
         return None
     newlist = []
-    newlist.append([replace if x == search else x for x in my_list])
+    newlist = (([replace if x == search else x for x in my_list]))
     return newlist
