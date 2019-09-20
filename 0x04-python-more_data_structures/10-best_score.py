2c8cbfe1a326f9b69bdc3f74af4d9642cbd25c6e
jalondono <juanlondono151776@hotmail.com>
2019-09-19 21:05:58 -0500

    upload task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    max  = 0
+    maxkey = ''
+    for i in a_dictionary:
+        if a_dictionary[i] > max:
+            max = a_dictionary[i]
+            maxkey = i
+    return maxkey
