899537fa0d56068722831de75be755bf17f5a808
jalondono <juanlondono151776@hotmail.com>
2019-09-19 22:40:34 -0500

    upload task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,20 @@ def roman_to_int(roman_string):
     if roman_string is None or not type(roman_string) is str:
         return None
     acum = 0
+    anterior = 0
+    j = 0
     dicc = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in roman_string:
         for letra in dicc:
             if i == letra:
-                acum += dicc[letra]
+                if j == 0:
+                    anterior = dicc[letra]
+                if anterior < dicc[letra]:
+                    acum = acum - anterior * 2 + dicc[letra]
+                else:
+                    acum += dicc[letra]
+                anterior = dicc[letra]
                 continue
+        j += 1
+
     return acum
