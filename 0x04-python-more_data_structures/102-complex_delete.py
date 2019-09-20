7916caf279b4dccfcdd5d1e43a47de5c7e606c1d
jalondono <juanlondono151776@hotmail.com>
2019-09-19 23:17:48 -0500

    upload task 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if not a_dictionary:
-        return None
+    if not a_dictionary or value not in a_dictionary.values():
+        return a_dictionary
     keys = []
     for i in a_dictionary:
         if a_dictionary[i] == value:
