64f89c0c1eb7f571a899c42f14f840b714b570ee
jalondono <juanlondono151776@hotmail.com>
2019-09-19 20:34:58 -0500

    upload task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    if a_dictionary is None:
+        return None
     newdic = a_dictionary.copy()
     for i in newdic:
         newdic.update({i: newdic.get(i) * 2})
