2b7bdb84ac1d61b79254f22257ecfd0cf6d38c19
jalondono <juanlondono151776@hotmail.com>
2019-09-19 22:49:18 -0500

    upload task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    num = 0
+    den = 0
+    if my_list is None:
+        return 0
+    for i in my_list:
+        num += i[0] * i[1]
+        den += i[1]
+    return num/den
