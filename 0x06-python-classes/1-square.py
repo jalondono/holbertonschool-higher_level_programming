bf076466e68ff986cb4ef60e8de1b98a435a07e0
jalondono <juanlondono151776@hotmail.com>
2019-10-01 17:33:07 -0500

    upload task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """"class square"""
+    def __init__(self, size=0):
+        """ Creates a private instance attribute, size
+               Args:
+                   - size: size of square
+        """
+        self.__size = size
