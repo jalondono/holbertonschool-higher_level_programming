270ce49f266fd958aa538ab7fdfbc3772c34df98
jalondono <juanlondono151776@hotmail.com>
2019-10-01 18:37:35 -0500

    upload task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python3
+class Square:
+    """class square"""
+    def __init__(self, size=0):
+
+        """ Creates a private instance attribute, size(int)
+        Args:
+            - size: size of square
+        Raises:
+            - TypeError: must be an int
+            - ValueError: must be >= 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
