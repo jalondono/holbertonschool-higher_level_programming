6e413cf6be3f35972e17e6b0c2432ebcba89065a
jalondono <juanlondono151776@hotmail.com>
2019-10-05 14:17:07 -0500

    quit conditional size = 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,18 +1,22 @@
-# !/usr/bin/python3
-"""
+#!/usr/bin/python3
+"""Module to print a square with the character #
+    Args:
+        size (int): size length of the square
 """
 
 
 def print_square(size):
-    """
+    """Function that prints a square with the character #
+        Return: printed square
     """
     if not isinstance(size, int):
         raise TypeError('size must be an integer')
-    elif size < 0:
-        raise ValueError('size must be >= 0')
-
     if isinstance(size, float) and size < 0:
         raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
 
     for i in range(size):
-        print('#' * size)
+        for j in range(size):
+            print('#', end='')
+        print()
