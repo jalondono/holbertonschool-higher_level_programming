5686db92c0d8fac1eb169e98c81ced26f76e48fe
jalondono <juanlondono151776@hotmail.com>
2019-10-05 14:06:16 -0500

    quit conditional size = 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,19 +7,14 @@ The 4-print_square module supplies one function, print_square(size).
 
 
 def print_square(size):
-    """print_square function
-        Args:  size is the size length of the square
-        Prints:  a square """
-    if not isinstance(size, int):
+    """ Doc """
+    if type(size) is not int:
         raise TypeError("size must be an integer")
-    elif size < 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
-    if isinstance(size, float) and size < 0:
-        raise TypeError("must be an integer")
-    if size == 0:
-        pass
-    size = int(size)
-    for i in range(0, size):
-        for j in range(0, size):
-            print("#", end='')
-        print()
+    for r in range(0, size):
+        for c in range(0, size):
+            if c != (size - 1):
+                print("#", end="")
+            else:
+                print("#")
