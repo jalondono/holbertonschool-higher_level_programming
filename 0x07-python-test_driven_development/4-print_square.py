f98f42cf283dcacc94ec3cee455559c9eed853f0
jalondono <juanlondono151776@hotmail.com>
2019-10-05 13:58:35 -0500

    quit conditional size = 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -16,6 +16,8 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if isinstance(size, float) and size < 0:
         raise TypeError("must be an integer")
+    if size == 0:
+        pass
     size = int(size)
     for i in range(0, size):
         for j in range(0, size):
