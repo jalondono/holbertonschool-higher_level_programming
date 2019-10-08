8889260ff5cf241e1030896093dbcd83493c4aef
jalondono <juanlondono151776@hotmail.com>
2019-10-07 22:11:11 -0500

    changed setter and getter

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -11,44 +11,30 @@ class Rectangle:
 
     @property
     def width(self):
-        """
-        Return:
-            - width value
-        """
         return self.__width
 
     @width.setter
-    def width(self, value):
-        """
-            Set the width value
-            ARGS:
-                - value
-        """
-        if not isinstance(self.__width, int):
-            raise TypeError("width must be an integer")
-        if self.__width < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+
+        if width < 0:
+            raise ValueError('width must be >= 0')
+
+        self.__width = width
 
     @property
     def height(self):
-        """
-        Return:
-            - height value
-        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Set the height value
-        ARGS:
-            - value
-        """
-        if not isinstance(self.__height, int):
-            raise TypeError("height must be an integer")
-        if self.__height < 0:
-            raise ValueError("height must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+
+        if value < 0:
+            raise ValueError('height must be >= 0')
+
         self.__height = value
 
     def area(self):
