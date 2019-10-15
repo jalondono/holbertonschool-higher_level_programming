642c6adfb0dbc8b1e50c352e72450237aa544b7c
jalondono <juanlondono151776@hotmail.com>
2019-10-15 17:33:26 -0500

    upload task 7

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
+    """
+    function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly)
+    :param obj:
+    :param a_class:
+    :return:
+    """
     if issubclass(type(obj), a_class):
         if type(obj) == a_class:
             return False
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """
+    base class
+    """
+    def area(self):
+        """
+        an exception sayind that methoth is
+        not implemented
+        :return:
+        """
+        raise Exception("area() is not implemented")
