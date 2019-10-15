f0bd80c3a55e7b1978eb8055ebbf3703c8f21823
jalondono <juanlondono151776@hotmail.com>
2019-10-15 17:00:59 -0500

    upload task 4 and 5

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    if issubclass(type(obj), a_class):
+        if type(obj) == a_class:
+            return False
+        return True
+    return False
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """
+    super class or base class
+    """
+    pass
