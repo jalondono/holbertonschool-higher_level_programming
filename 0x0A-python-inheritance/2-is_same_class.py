e77780e9c6cc537486e143030cab2efbfd56be01
jalondono <juanlondono151776@hotmail.com>
2019-10-15 16:27:01 -0500

    upload task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    """
+    function to identify to which class belong an object
+    :param obj:
+    :param a_class:
+    :return:
+    """
+    if type(obj) is a_class:
+        return True
+    return False
