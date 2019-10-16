3c7f6aa07fb0bda7f592674ed3749ac1f6a41e4b
jalondono <juanlondono151776@hotmail.com>
2019-10-15 23:17:29 -0500

    upload advanced task 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+This is the "101-add_attribute" module.
+The 101-add_attribute module supplies one function, add_attribute.
+"""
+
+
 def add_attribute(obj, name, value):
+    """add an atribute to a object only if its posible"""
     if hasattr(obj, name):
         raise TypeError("can't add new attribute")
     else:
