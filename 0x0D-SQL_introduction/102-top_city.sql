f74984a055ed908748661a9a70ae88f8c9cb91eb
jalondono <juanlondono151776@hotmail.com>
2019-11-25 17:43:05 -0500

    upload advanced  102 task

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- script that lists all records of the table second_table
+SELECT city,AVG(value) AS avg_temp FROM temperatures WHERE month BETWEEN 7 AND 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
