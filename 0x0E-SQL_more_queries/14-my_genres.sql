c1eb0e8152f636b19bef444d23ae623d16f62892
jalondono <juanlondono151776@hotmail.com>
2019-11-26 17:11:35 -0500

    ADD TASK 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
- -- to lists all genres of the show Dexter.
-SELECT name FROM tv_shows A join tv_show_genres B ON A.id = B.show_id join tv_genres C ON C.id = B.genre_id WHERE A.title = 'Dexter' ORDER BY C.name;
+-- to lists all genres of the show Dexter.
+SELECT name FROM tv_shows A JOIN tv_show_genres B ON A.id = B.show_id JOIN tv_genres C ON C.id = B.genre_id WHERE A.title = 'Dexter' ORDER BY C.name;
---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,2 @@
+ -- to lists all genres of the show Dexter.
+SELECT name FROM tv_shows A join tv_show_genres B ON A.id = B.show_id join tv_genres C ON C.id = B.genre_id WHERE A.title = 'Dexter' ORDER BY C.name;
