5269e556f120bebbf480094cff7f4fb6e670867f
jalondono <juanlondono151776@hotmail.com>
2019-11-26 14:11:33 -0500

    upload task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
--- lists all cities contained in the database hbtn_0d_usa
-
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT A.title, B.genre_id from tv_shows A JOIN tv_show_genres B ON A.id = B.show_id ORDER BY A.title, B.genre_id;
 
