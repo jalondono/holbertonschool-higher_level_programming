470e2f3dfc71f566f0042c113494bc73cbe26c98
jalondono <juanlondono151776@hotmail.com>
2019-11-26 14:12:48 -0500

    fIXED UPPER CASE TASK 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,2 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT A.title, B.genre_id from tv_shows A JOIN tv_show_genres B ON A.id = B.show_id ORDER BY A.title, B.genre_id;
-
+SELECT A.title, B.genre_id FROM tv_shows A JOIN tv_show_genres B ON A.id = B.show_id ORDER BY A.title, B.genre_id;
