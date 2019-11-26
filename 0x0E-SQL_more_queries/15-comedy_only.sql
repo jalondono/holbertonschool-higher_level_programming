d1187c9e1f3993505a2623d516206eabe635f164
jalondono <juanlondono151776@hotmail.com>
2019-11-26 17:21:51 -0500

    fixed upper case 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,2 +1,2 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshow
-SELECT title FROM tv_show_genres A join tv_genres B ON A.genre_id = B.id join tv_shows C ON C.id = A.show_id WHERE B.name = 'Comedy' ORDER BY C.title;
+SELECT title FROM tv_show_genres A JOIN tv_genres B ON A.genre_id = B.id join tv_shows C ON C.id = A.show_id WHERE B.name = 'Comedy' ORDER BY C.title;
