ad234d12d4a4368e1ff994ecfb21b186a0fef329
jalondono <juanlondono151776@hotmail.com>
2019-12-14 16:03:53 -0500

    fixed task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,24 +3,24 @@
 takes in an argument and displays all values in
 the states table of hbtn_0e_0_usa where name matches the argument.
 """
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
 
-    m_name = argv[1]
-    m_passwd = argv[2]
-    m_db = argv[3]
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=m_name,
-                         passwd=m_passwd,
-                         db=m_db)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states "
-                "WHERE name LIKE '{:s}' "
-                "ORDER BY states.id".format(argv[4]))
-    rows = cur.fetchall()
-    for data in rows:
-        print(data)
-    cur.close()
-    db.close()
+import MySQLdb
+from sys import argv
+
+m_name = argv[1]
+m_passwd = argv[2]
+m_db = argv[3]
+db = MySQLdb.connect(host='localhost',
+                     port=3306,
+                     user=m_name,
+                     passwd=m_passwd,
+                     db=m_db)
+cur = db.cursor()
+cur.execute("SELECT * FROM states "
+            "WHERE name LIKE '{:s}' "
+            "ORDER BY states.id".format(argv[4]))
+rows = cur.fetchall()
+for data in rows:
+    print(data)
+cur.close()
+db.close()
