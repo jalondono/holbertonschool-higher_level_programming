8e5a2705e5270c5ba3a2686b2504cd9e1afe40b9
jalondono <juanlondono151776@hotmail.com>
2019-12-13 19:40:40 -0500

    upload until task 5

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+import MySQLdb
+from sys import argv
+
+size = len(argv) - 1
+if size == 3:
+    m_name = argv[1]
+    m_passwd = argv[2]
+    m_db = argv[3]
+    db = MySQLdb.connect(host='localhost', user=m_name, passwd=m_passwd, db=m_db)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id;")
+    rows = cur.fetchall()
+    for data in rows:
+        print(data)
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+import MySQLdb
+from sys import argv
+
+size = len(argv) - 1
+if size == 4:
+    m_name = argv[1]
+    m_passwd = argv[2]
+    m_db = argv[3]
+    db = MySQLdb.connect(host='localhost', user=m_name, passwd=m_passwd, db=m_db)
+    cur = db.cursor()
+    cur.execute("SELECT B.name FROM states A JOIN cities B ON A.id = B.state_id AND A.name = %(username)s ORDER BY B.id", {'username': argv[4]})
+    rows = cur.fetchall()
+    for row in range(len(rows)):
+        if row != len(rows) - 1:
+            print('{}, '.format(rows[row][0]), end='')
+        else:
+            print('{}'.format(rows[row][0]))
