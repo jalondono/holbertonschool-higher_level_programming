da6275b6816906bfcc654241df0b28e8667f032a
jalondono <juanlondono151776@hotmail.com>
2019-12-15 12:55:00 -0500

    pep8 task 14 city model

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,6 +12,7 @@ Base = declarative_base()
 
 class City(Base):
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
