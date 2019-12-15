933c9ec31cf4f02b07cf661fb2b3fbc757c2780e
jalondono <juanlondono151776@hotmail.com>
2019-12-14 21:27:56 -0500

    fixed pep8

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,25 +3,24 @@
 takes in an argument and displays all values in the states
 table of hbtn_0e_0_usa where name matches the argument.
 """
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
 
-import MySQLdb
-from sys import argv
-
-
-m_name = argv[1]
-m_passwd = argv[2]
-m_db = argv[3]
-db = MySQLdb.connect(host='localhost',
-                     port=3306,
-                     user=m_name,
-                     passwd=m_passwd,
-                     db=m_db)
-cur = db.cursor()
-cur.execute("SELECT * FROM states "
-            "WHERE name = %(username)s "
-            "ORDER BY id", {'username': argv[4]})
-rows = cur.fetchall()
-for data in rows:
-    print(data)
-cur.close()
-db.close()
+    m_name = argv[1]
+    m_passwd = argv[2]
+    m_db = argv[3]
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=m_name,
+                         passwd=m_passwd,
+                         db=m_db)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states "
+                "WHERE name = %(username)s "
+                "ORDER BY id", {'username': argv[4]})
+    rows = cur.fetchall()
+    for data in rows:
+        print(data)
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,24 +3,24 @@
 lists all cities from the database hbtn_0e_4_usa
 """
 
-import MySQLdb
-from sys import argv
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
 
-
-m_name = argv[1]
-m_passwd = argv[2]
-m_db = argv[3]
-db = MySQLdb.connect(host='localhost',
-                     port=3306,
-                     user=m_name,
-                     passwd=m_passwd,
-                     db=m_db)
-cur = db.cursor()
-cur.execute("SELECT B.id, B.name, A.name "
-            "FROM states A JOIN cities B ON A.id = B.state_id "
-            "ORDER BY B.id")
-rows = cur.fetchall()
-for data in rows:
-    print(data)
-cur.close()
-db.close()
+    m_name = argv[1]
+    m_passwd = argv[2]
+    m_db = argv[3]
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=m_name,
+                         passwd=m_passwd,
+                         db=m_db)
+    cur = db.cursor()
+    cur.execute("SELECT B.id, B.name, A.name "
+                "FROM states A JOIN cities B ON A.id = B.state_id "
+                "ORDER BY B.id")
+    rows = cur.fetchall()
+    for data in rows:
+        print(data)
+    cur.close()
+    db.close()
