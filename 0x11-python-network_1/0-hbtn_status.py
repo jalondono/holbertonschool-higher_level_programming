9f4c80c95024de31a6705a1f2319fd90fd18ea63
jalondono <juanlondono151776@hotmail.com>
2020-01-13 13:02:42 -0500

    Fixed pep8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import urllib.request
 """Write a Python script that
 fetches https://intranet.hbtn.io/status"""
 
 if __name__ == "__main__":
+    import urllib.request
     req = urllib.request.Request('https://intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
