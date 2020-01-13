5ae19048bab36401547829992abc71bd91a34195
jalondono <juanlondono151776@hotmail.com>
2020-01-13 16:32:47 -0500

    upload task 101

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3`
+import requests
+from sys import argv
+"""Write a Python script that takes in a string
+and sends a search request to the Star Wars API"""
+if __name__ == "__main__":
+
+    name = argv[1]
+    list_names = []
+    url = 'https://swapi.co/api/people/?search=' + name
+    r = requests.get(url)
+    output = r.json()
+    count = output.get('count')
+    results = output.get('results')
+    for names in results:
+        list_names.append(names['name'])
+    while output.get('next'):
+        r = requests.get(output.get('next'))
+        output = r.json()
+        results = output.get('results')
+        for names in results:
+            list_names.append(names['name'])
+    print("Number of results: {:d}".format(count))
+    for name in list_names:
+        print(name)
