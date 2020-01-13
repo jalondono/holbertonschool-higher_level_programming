0fd2c4eb170a67f19fdaa9ef0ed90d0f42152a1c
jalondono <juanlondono151776@hotmail.com>
2020-01-13 16:43:37 -0500

    quit garbage print

--- 0x11-python-network_1/101-starwars.py
+++ 0x11-python-network_1/101-starwars.py
@@ -5,20 +5,23 @@ from sys import argv
 and sends a search request to the Star Wars API"""
 if __name__ == "__main__":
 
-    name = argv[1]
-    list_names = []
-    url = 'https://swapi.co/api/people/?search=' + name
-    r = requests.get(url)
-    output = r.json()
-    count = output.get('count')
-    results = output.get('results')
-    print("Number of results: {:d}".format(count))
-    print()
-    for names in results:
-        print(names['name'])
-    while output.get('next'):
-        r = requests.get(output.get('next'))
+    if len(argv) != 2:
+        name = ""
+        print("No result")
+    else:
+        name = argv[1]
+        list_names = []
+        url = 'https://swapi.co/api/people/?search=' + name
+        r = requests.get(url)
         output = r.json()
+        count = output.get('count')
         results = output.get('results')
+        print("Number of results: {:d}".format(count))
         for names in results:
-            print(names['name'])
+            print(names.get("name"))
+        while output.get('next'):
+            r = requests.get(output.get('next'))
+            output = r.json()
+            results = output.get('results')
+            for names in results:
+                print(names.get("name"))
