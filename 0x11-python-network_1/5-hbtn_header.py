7c1f27ee0f7f341c3b9c5ac3e2c5c8eb0f0b27fc
jalondono <juanlondono151776@hotmail.com>
2020-01-13 13:55:40 -0500

    upload task 6

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,9 +2,9 @@
 import requests
 from sys import argv
 
-"""Write a Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as
- a parameter, and finally displays the body of the response."""
+""""Write a Python script that takes in a URL,
+ sends a request to the URL and displays the
+  value of the variable X-Request-Id"""
 
 if __name__ == "__main__":
     header = 'X-Request-Id'
