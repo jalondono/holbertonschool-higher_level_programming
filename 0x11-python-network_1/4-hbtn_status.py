ef404b17ce3c05e2aecda186c2d9bded609e758b
jalondono <juanlondono151776@hotmail.com>
2020-01-13 13:32:18 -0500

    added body response

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,6 @@ import requests
  https://intranet.hbtn.io/status"""
 if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
     print("\t type: {}".format(type(r.text)))
     print("\t content: {}".format(r.text))
