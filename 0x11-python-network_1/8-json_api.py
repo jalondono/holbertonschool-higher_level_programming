007eff8dd19be3219e723bcbaeafe355eab76b05
jalondono <juanlondono151776@hotmail.com>
2020-01-13 14:40:50 -0500

    upload task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+"""Write a Python script that takes
+ in a letter and sends a POST request to
+  http://0.0.0.0:5000/search_user with the letter as
+   a parameter.
+"""
+
+if __name__ == "__main__":
+    letter = ""
+    if argv[1]:
+        letter = argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    values = {'q': letter}
+    try:
+        r = requests.post(url, values)
+        output = r.json()
+        if output:
+            print("[{}] {}".format(output.get('id'), output.get('name')))
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")
