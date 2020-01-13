5b13c82a3df19df34c17539daba2b54e06d84b02
jalondono <juanlondono151776@hotmail.com>
2020-01-13 15:47:18 -0500

    upload task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+import requests
+from sys import argv
+
+"""Get request to get the last commits of a github repository"""
+
+if __name__ == "__main__":
+    repo_name = argv[1]
+    owner_name = argv[2]
+    param = {"per_page": "10"}
+    url = "https://api.github.com/repos/" + owner_name + "/" + repo_name + "/commits"
+    response = requests.get(url, params=param)
+    output = response.json()
+    for commit in output:
+        sha = commit['sha']
+        author = commit['commit']['author']['name']
+        print("{}: {}".format(sha, author))
