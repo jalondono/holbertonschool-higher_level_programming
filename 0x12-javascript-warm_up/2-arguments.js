727163e538ee81ee67cbad3bb01b8c4c5a1c3b5b
jalondono <juanlondono151776@hotmail.com>
2020-02-04 16:32:38 -0500

    Upload task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,8 +3,10 @@
  * Function to Check if were passed args
  */
 const args = process.argv;
-if (args.length > 2) {
+if (args.length === 2) {
   console.log('Argument found');
+} else if (args.length === 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
