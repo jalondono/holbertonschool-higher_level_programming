318f92992ec7c285ba11e8faf5407140c6244e81
jalondono <juanlondono151776@hotmail.com>
2020-02-04 22:06:14 -0500

    upload task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 function factorial (a) {
-  if (isNaN(a)) {
-    return NaN;
+  if (!a || isNaN(a)) {
+    return (1);
   }
-  if (a < 1) {
+  if (a === 1) {
     return (1);
   }
   return a * (factorial(a - 1));
 }
-const a = parseInt(process.argv[2]);
-let b = 0;
-b = factorial(a);
-console.log(b);
+console.log(factorial(parseInt(process.argv[2])));
