93900c4d82aaced8cbdff2304b02be1761034d4a
jalondono <juanlondono151776@hotmail.com>
2020-02-10 21:56:09 -0500

    upload task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    let j = 0;
+    let letter = 'X';
+    for (i = 0; i < this.height; i++) {
+      letter = '';
+      for (j = 0; j < this.width; j++) {
+        letter += 'X';
+      }
+      console.log(letter);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
