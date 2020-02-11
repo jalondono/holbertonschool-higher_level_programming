f83d6cc2c329f3c71c457592b1c05f857f090062
jalondono <juanlondono151776@hotmail.com>
2020-02-11 15:55:09 -0500

    upload task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,9 +2,9 @@
 const Request = require('request');
 const fs = require('fs');
 const args = process.argv;
-Request (args[2], (err, response, body) => {
+Request(args[2], (err, response, body) => {
   if (!err) {
-    fs.writeFileSynk(args[3], body, function(err) {
+    fs.writeFile(args[3], body, 'utf-8', function (err) {
       if (err) {
         return console.error(err);
       }
