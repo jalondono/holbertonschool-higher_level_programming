1a6712008465e9bf218970eab58b54b4d577844a
jalondono <51680387+jalondono@users.noreply.github.com>
2020-02-11 22:57:34 -0500

    Update task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,25 @@
 #!/usr/bin/node
-const Request = require('request');
-const args = process.arg;
-Request('https://jsonplaceholder.typicode.com/todos', (err, response, body) => {
-    data = JSON.parse(body);
-    console.log();
-})
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const results = JSON.parse(body);
+    const dict = {};
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].userId in dict) {
+        if (results[i].completed) {
+          dict[results[i].userId] = dict[results[i].userId] + 1;
+        }
+      } else {
+        if (results[i].completed) {
+          dict[results[i].userId] = 1;
+        }
+      }
+    }
+    console.log(dict);
+  }
+});
